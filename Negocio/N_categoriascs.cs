4bc415120c326cba77ca876530bec501548a7392
Marcos Aguirre Develop_Programing <mjaguirre5@gmail.com>
2022-07-04 12:01:32 -0700

    Add files via upload

---
+++ Negocio/N_categoriascs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Datos;
+using Entidades;
+
+namespace Negocio
+{
+    public  class N_categoriascs
+    {
+        public static DataTable Listar_ca(String cTexto)
+        {
+            D_categorias Datos = new D_categorias();
+            return Datos.Listar_ca(cTexto);
+
+
+
+        }
+        public static string Guardar_ca (int Opcion, E_categorias oca)
+        {
+            D_categorias Datos = new D_categorias();
+            return Datos.Guardar_ca(Opcion, oca);
+            //drop_ca
+        }
+        public static string drop_ca(int Codigo)
+        {
+            D_categorias Datos = new D_categorias();
+
+            return Datos.drop_ca(Codigo);
+            //drop_ca
+        }
+
+    }
+}
