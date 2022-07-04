07571bb44bc9fd7c693cb8bdce57fcb47dc9f91d
Marcos Aguirre Develop_Programing <mjaguirre5@gmail.com>
2022-07-04 11:57:56 -0700

    Add files via upload

---
+++ Datos/D_categorias.cs
@@ -0,0 +1,125 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Entidades;
+
+namespace Datos
+{
+    public class D_categorias
+
+    {
+        public DataTable Listar_ca(String @cTexto)
+        {
+            SqlDataReader rdr;
+            DataTable mydata = new DataTable();
+            SqlConnection sqlcon = new SqlConnection();
+            try
+            {
+                sqlcon = conexion.GetInstancia().creaconexion();
+                SqlCommand cmd = new SqlCommand("Usp_Listado_ca", sqlcon);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@cTexto", SqlDbType.VarChar).Value = cTexto;
+                sqlcon.Open();
+                rdr = cmd.ExecuteReader();
+                mydata.Load(rdr);
+                return mydata;
+
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                if (sqlcon.State == ConnectionState.Open) sqlcon.Close();
+            }
+
+
+        }
+        public string Guardar_ca(int Opcion, E_categorias oca)
+        {
+            String Rpta;
+            SqlConnection Sqlcon = new SqlConnection();
+
+            try
+            {
+                Sqlcon = conexion.GetInstancia().creaconexion();
+                SqlCommand commandd = new SqlCommand("USP_Guardar_ca", Sqlcon);
+                commandd.CommandType = CommandType.StoredProcedure;
+                commandd.Parameters.Add("@Opcion", SqlDbType.Int).Value = Opcion;
+                commandd.Parameters.Add("@Codigo_ca", SqlDbType.Int).Value = oca.codigo_ca;
+                commandd.Parameters.Add("@Desc_ca", SqlDbType.VarChar).Value = oca.desc_ca;
+                Sqlcon.Open();
+                Rpta = commandd.ExecuteNonQuery() == 1 ? "ok" : "no se pudo registrar el proceso";
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                Rpta = ex.Message;
+                throw;
+            }
+            finally
+            {
+                if (Sqlcon.State == ConnectionState.Open) Sqlcon.Close();
+
+            }
+            return Rpta;
+
+        }
+        public string drop_ca(int Codigo)
+        {
+            String Rpta;
+            SqlConnection Sqlcon = new SqlConnection();
+
+            try
+            {
+                Sqlcon = conexion.GetInstancia().creaconexion();
+                SqlCommand commandd = new SqlCommand("Usp_Eliminar_ca", Sqlcon);
+                commandd.CommandType = CommandType.StoredProcedure;
+                commandd.Parameters.Add("@Codigo", SqlDbType.Int).Value = Codigo;
+
+                Sqlcon.Open();
+                Rpta = commandd.ExecuteNonQuery() == 1 ? "ok" : "no se pudo Elimar el Registro";
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                Rpta = ex.Message;
+                throw;
+            }
+            finally
+            {
+                if (Sqlcon.State == ConnectionState.Open) Sqlcon.Close();
+
+            }
+            return Rpta;
+
+
+
+
+
+
+
+
+
+
+
+            // return Lista_ca()
+        }
+    }
+}
---
+++ Datos/conexion.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public  class conexion
+    {
+        private string Base;
+        private string user;
+        private string server;
+        private string clave;
+        private static conexion Con = null;
+        private conexion()
+        {
+            this.Base = "mydata";
+            this.user = "sa";
+            this.server = "MJDEVELOP\\SQLEXPRESS";
+            this.clave = "datasql";
+
+           
+
+        }
+        public SqlConnection creaconexion()
+        {
+            SqlConnection cadena = new SqlConnection();
+           
+            try
+            {
+              //  cadena.Open();
+                cadena.ConnectionString = "Server=" + this.server + "; Database=" + this.Base + ";User Id=" + this.user +";Password="+this.clave;
+
+
+            }
+            catch (Exception ex)
+            {
+                cadena = null;
+
+                throw ex;
+            } return cadena;
+
+        }
+       public static conexion GetInstancia ()
+        {
+            if(Con == null)
+            {
+                Con = new conexion();
+
+            }
+            return Con;
+
+        }
+
+    }
+   
+}
