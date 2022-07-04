90e07ab9d8d3c2b4956c76aa0aa9569636cbc8d1
Marcos Aguirre Develop_Programing <mjaguirre5@gmail.com>
2022-07-04 12:00:06 -0700

    Entidades

---
+++ Entidades/E_categorias.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public  class E_categorias
+    {
+        public int codigo_ca { get; set; }
+        public string desc_ca { get; set; }
+    }
+}
